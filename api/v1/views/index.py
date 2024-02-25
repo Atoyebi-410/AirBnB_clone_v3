74fad1b904ac05ee19a37f4223cc2b02b08d9156
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-02-25 10:01:01 +0000

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,21 @@
 #!/usr/bin/python3
-"""
-index
-"""
-
-from flask import jsonify
+""" Module containing views """
 from api.v1.views import app_views
-
+from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """
-    status route
-    :return: response with json
-    """
-    data = {
-        "status": "OK"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    """ returns a JSON """
+    status = {"status": "OK"}
+    return jsonify(status)
 
 
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+@app_views.route("/stats", strict_slashes=False)
 def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    """ retrieves the number of each objects by type """
+    class_dict = {"Amenity": "amenities", "City": "cities", "Place": "places",
+                  "Review": "reviews", "State": "states", "User": "users"}
+    objs = {class_dict[cls]: storage.count(cls) for cls in class_dict}
+    return jsonify(objs)
