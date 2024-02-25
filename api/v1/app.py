997225eb1a0b6dfd825a2cefbc294eb8f685b738
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-02-25 09:27:48 +0000

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,10 +13,10 @@ from models import storage
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
-
+app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -42,4 +42,6 @@ def handle_404(exception):
     return(resp)
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    PORT = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
