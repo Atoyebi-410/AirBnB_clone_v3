87be576e29f33760d51f50b22a6feceed88f732a
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-02-25 08:20:11 +0000

    status of your api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
-""" Module containing Flask application """
+"""
+app
+"""
+
 from flask import Flask, jsonify
 from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
 from os import getenv
 
+from api.v1.views import app_views
+from models import storage
+
+
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    """ closes the storage """
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(exception):
-    """ Returns a JSON formatted 404 status code response """
-    return jsonify({'error': 'Not found'}), 404
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST")
-    if host is None:
-        host = '0.0.0.0'
-    port = getenv("HBNB_API_PORT")
-    if port is None:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
